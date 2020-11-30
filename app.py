022fce186461a906e72c92ebd9c0386814100fba
Rajasekar M <rajasekar.1999@gmail.com>
2020-11-30 23:37:08 +0530

    #3

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request,redirect,url_for
 import dbops
 
-app = Flask(__name__, template_folder='templates')s
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def Home():
